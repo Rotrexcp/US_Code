1ce4c3dee25751469d5c2d787d743c522aedffef
=Rotrex3 <=rodrigocastillap@gmail.com>
2024-11-28 12:30:58 +0100

    centros sanitarios empezado

---
+++ Python/2_Laboratorio/7_centros_sanitarios/src/coordenadas.py
@@ -0,0 +1,16 @@
+from typing import NamedTuple
+from math import sqrt
+
+Coordenadas = NamedTuple("Coordenadas", ("latitud", float),("longitud", float))
+
+def calcular_distancia(coord1: Coordenadas, coord2: Coordenadas) -> float:
+    distancia_euclidea = sqrt((coord2.latitud - coord1.latitud)**2 + (coord2.longitud - coord1.longitud)**2)
+    
+    return distancia_euclidea
+
+
+def calcular_media_coordenadas(coordenadas: list[Coordenadas])->Coordenadas:
+    latitud_media = sum(coord.latitud for coord in coordenadas) / len(coordenadas)
+    longitud_media = sum(coord.longitud for coord in coordenadas) / len(coordenadas)
+    
+    return Coordenadas(latitud_media, longitud_media)
