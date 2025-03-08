0c20dffbbbdd4abc8ea391554ce5787e9d74f8a4
Rodrigo Castilla Pousa <rodcaspou@alum.us.es>
2025-03-08 12:28:16 +0100

    pull done

---
+++ Java/__pruebas/src/main/LocalDate.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.time.Month;
+
+public class LocalDate {
+
+	public static void main(String[] args) {
+		//LocalDate fecha = new LocalDate.of(2023, Month.APRIL, 23);
+	}
+
+}
